fa1c744f029832728f7b7b67a9e648b215ffc319
a-kalleri <ansonkalleri@gmail.com>
2026-08-13 18:01:05 +0530

    registers

--- main.c
+++ main.c
@@ -1,7 +1,142 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define REG_EMPTY UINT8_MAX
+
+typedef struct {
+
+        uint8_t lhs;
+        uint8_t rhs;
+        uint8_t op;
+        uint8_t acc;
+        uint8_t mem;
+
+} registers_t;
+
+void reset_reg (registers_t *reg, int empty_mem) {
+
+        reg -> lhs = REG_EMPTY;
+        reg -> rhs = REG_EMPTY;
+        reg -> op = REG_EMPTY;
+
+        if (empty_mem) reg -> mem = REG_EMPTY;
+
+}
+
+int mov_operand (registers_t reg, int value) {
+
+        if (reg.lhs == REG_EMPTY) {
+                reg.lhs = value;
+        } else {
+                reg.rhs = value;
+        }
+
+        return 0;
+
+}
+
+int mov_values (registers_t reg, int c) {
+
+        switch (c) {
+
+        case '1':
+                mov_operand(reg, 1);
+                break;
+
+        case '0':
+                mov_operand(reg, 0);
+                break;
+
+        case '|':
+                reg.op = 1;
+                break;
+
+        case '&':
+                reg.op = 2;
+                break;
+        
+        case '^':
+                reg.op = 3;
+                break;
+        
+        case '!':
+                reg.op = 4;
+                break;
+ 
+        default:
+                return 101;
+
+        }
+
+        return 0;
+
+}
+
+int evaluate (registers_t *reg) {
+
+        if (reg -> op == REG_EMPTY) {
+                return 0;
+        }
+
+        printf("%d\n", reg -> op);
+
+        switch (reg -> op) {
+
+        case 1:
+                reg -> acc = reg -> lhs || reg -> rhs;
+                reset_reg(reg, 0);
+                break;
+        
+        case 2:
+                reg -> acc = reg -> lhs && reg -> rhs;
+                reset_reg(reg, 0);
+                break;
+        
+        case 3:
+                reg -> acc = reg -> lhs != reg -> rhs;
+                reset_reg(reg, 0);
+                break;
+        
+        case 4:
+                reg -> acc = !reg -> lhs;
+                reset_reg(reg, 0);
+                break;
+        
+        default:
+                return 102;
+
+        }
+
+        return 0;
+
+}
+
+int resolve_errorcodes (int code, int c) {
+
+        switch (code) {
+
+        case 101:
+                fprintf(stderr, "BYTE ERROR: '%c' is not recognized.\nAbort.\n", c);
+                return 0;
+                break;
+        
+        case 102:
+                fprintf(stderr, "EVALUATE ERROR: encountered an unknown operator.\nAbort.\n");
+                return 1;
+                break;
+ 
+        }
+
+        return 0;
+
+}
+
 int main (void) {
+
+        registers_t reg;
+
+        reset_reg(&reg, 1);
+        reg.acc = REG_EMPTY;
 
         FILE *fp = fopen("test", "rb");
         if (fp == NULL) {
@@ -9,65 +144,32 @@ int main (void) {
         }
 
         int c = -1;
-        int lhs = -1;
-        int rhs = -1;
-        int op = -1;
-        int acc = -1;
-        int mem = -1;
-
-        int assign = 0;
-
         while ((c = fgetc(fp)) != EOF) {
 
-                switch(c) {
-                        case '1':
-                        case '0': {
-                                int val = c - '0';
-                                if (lhs == -1) {
-                                        lhs = val;
-                                } else {
-                                        rhs = val;
-                                }
-                                break;
-                        }
-                        case '|':
-                                op = 0;
-                                break;
-                        case '&':
-                                op = 1;
-                                break;
-                        case '-':
-                                if (assign == 0) assign++;
-                                continue;
-                                break;
-                        case '>':
-                                if (assign == 1) assign++;
-                                continue;
-                                break;
-                        case 'a':
-                                if (assign == 2 && acc != -1) mem = acc;
-                                assign = 0;
-                                break;
-                        default:
-                                break;
+                if (
+                        c == ' '||
+                        c == '\t'||
+                        c == '\n'||
+                        c == '\r'
+                ) {
+                        continue;
                 }
 
-                if (lhs != -1 && rhs != -1 && op != -1) {
-                        if (op == 0) {
-                                acc = lhs || rhs;
-                        } else {
-                                acc = lhs && rhs;
-                        }
-
-                        lhs = acc;
-                        rhs = -1;
-                        op = -1;
+                int result = mov_values(reg, c);
+                if (resolve_errorcodes(result, c)) {
+                        return result;
                 }
+
+                result = evaluate(&reg);
+                if (resolve_errorcodes(result, c)) {
+                        return result;
+                }
+
+                printf("last op: %d\n", reg.op);
 
         }
 
-        printf("acc: %d", acc);
-        if (mem != -1) printf("mem: %d", mem);
+        printf("acc: %d\n", reg.acc);
 
         return 0;
 
